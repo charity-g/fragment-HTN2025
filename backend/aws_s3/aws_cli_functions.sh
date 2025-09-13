532c94397e92cdb15ed3864818effbfb8470c51b
paultibe <paultiberghien1@gmail.com>
2025-09-13 08:08:30 -0400

    fix s3 -> lambda wiring

--- backend/aws_s3/aws_cli_functions.sh
+++ backend/aws_s3/aws_cli_functions.sh
@@ -8,7 +8,6 @@ aws iam list-policies --query 'Policies[?PolicyName==`LambdaS3AccessPolicy`]'
 aws iam list-roles --query 'Roles[?RoleName==`LambdaS3Role`]'
 
 # update lambda
-zip -r lambda_function.zip main_setup_lambda_handler.py    
 aws lambda update-function-code --function-name ConvertWebMToVideoFormats --zip-file fileb://lambda_function.zip
 
 # check if lambda function exists
