99a16be00b2576af60fcb378437ef0fd42ce7d83
cherr <charity.grey1@gmail.com>
2025-09-13 03:16:25 -0400

    lambda works

--- aws_s3/lambda_convert.py
+++ aws_s3/lambda_convert.py
@@ -1,16 +1,25 @@
 # mp4
-import ffmpeg
+# import ffmpeg
 
-input_path = "./2025-02-09 20-58-31.webm"
-output_path = "output.mp4"
-
-ffmpeg.input(input_path).output(output_path, vcodec='libx264').run()
-
-# optional 
-# from moviepy.editor import VideoFileClip
-
-# input_path = "input.webm"
+# input_path = "./2025-02-09 20-58-31.webm"
 # output_path = "output.mp4"
 
-# clip = VideoFileClip(input_path)
-# clip.write_videofile(output_path, codec="libx264")
+# ffmpeg.input(input_path).output(output_path, vcodec='libx264').run()
+
+# moviepy
+
+from moviepy.editor import VideoFileClip
+
+input_path = "2025-02-09 20-58-31.webm"
+output_path = "output.mp4"
+
+clip = VideoFileClip(input_path)
+clip.write_videofile(output_path, codec="libx264")
+
+from moviepy.editor import VideoFileClip
+
+# Load your WebM video
+clip = VideoFileClip("2025-02-09 20-58-31.webm")
+
+# Export as GIF
+clip.write_gif("output.gif")
